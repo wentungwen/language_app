67fa20116d0f9b3b8ef2ce17bc966bb6ac4d5203
wentung <wentungwen@gmail.com>
2024-01-12 13:40:51 +0100

    feat: update the openai model

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,20 @@
 from flask import Flask, jsonify, request, session
 from flask_cors import CORS
-import googletrans
 from google.cloud import translate_v2 as translate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+from openai import OpenAI
 import secrets
 import jwt
 from datetime import datetime
-import requests, os
+import requests, os, json
 from datetime import datetime
-
-# import psycopg2
 
 
 ## GPT & google translate
 GPT_APIKEY = os.environ.get("GPT_APIKEY")
-GPT_API_ENDPOINT = "https://api.openai.com/v1/completions"
+GPT_API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
 GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
 ## app and database config
@@ -29,6 +27,10 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.urandom(32)
 db = SQLAlchemy(app)
 
+# openai object
+ai_client = OpenAI(api_key=GPT_APIKEY)
+
+
 class User(db.Model):
     __tablename__ = "users"
     user_id = Column(Integer, primary_key=True)
@@ -36,6 +38,13 @@ class User(db.Model):
     email = Column(String(100), unique=True, nullable=True)
     password = Column(String(100))
     conversations = relationship("Conversation", back_populates="user")
+
+    def check_password(self, password):
+        return self.password == password
+
+    def set_password(self, new_password):
+        self.password = new_password
+        return True
 
 
 class Conversation(db.Model):
@@ -58,64 +67,83 @@ class Content(db.Model):
     conversation = relationship("Conversation", back_populates="contents")
 
 
-# mock data
-mock_conversations = [
-    {
-        "date": "2019-05-04",
-        "topic": "te laat",
-        "lan_code": "nl",
-        "conversations": [
-            {
-                "sender": "A",
-                "content": "(backend)Ik heb de treinkaarten. Twee retourtjes naar Den Haag.",
-            },
-            {
-                "sender": "B",
-                "content": "Volgens de man achter het loket, vertrekt de trein over een kwartier van spoor vijf.",
-            },
-            {
-                "sender": "A",
-                "content": "Goed zo. Ik ga even naar de wc. Weet jij waar die is?",
-            },
-            {
-                "sender": "B",
-                "content": "Ja. Daar bij de ingang. Ik blijf hier bij de koffers.",
-            },
-        ],
-    },
-    {
-        "date": "2019-05-05",
-        "topic": "de lein tiran ",
-        "lan_code": "nl",
-        "conversations": [
-            {"sender": "A", "content": "(backend)Mamma, waar is pappa?"},
-            {
-                "sender": "B",
-                "content": "Op kantoor, natuurlijk. Ga maar even naar buiten.",
-            },
-            {"sender": "A", "content": "Speel met poesje in de tuin. "},
-            {
-                "sender": "B",
-                "content": "Maar waarom niet op straat? Met de jongens.",
-            },
-        ],
-    },
-]
+# methods
 
 
+def generate_token(user_id):
+    payload = {"user_id": user_id}
+    token = jwt.encode(payload, app.config["SECRET_KEY"], algorithm="HS256")
+    return token
+
+
+def decode_token(token):
+    try:
+        payload = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
+        user_id = payload["user_id"]
+        print("Decoded payload:", payload)
+        return user_id
+    except jwt.ExpiredSignatureError:
+        print("Token has expired")
+        return None
+    except jwt.PyJWTError as e:
+        print("JWT decoding error:", str(e))
+        return None
+
+
+def translate_text(text, source_language, target_language):
+    translate_client = translate.Client.from_service_account_json(
+        GOOGLE_APPLICATION_CREDENTIALS
+    )
+    result = translate_client.translate(
+        text, source_language=source_language, target_language=target_language
+    )
+    return result["translatedText"]
+
+
+def add_content_to_conversation(conversation_id, content, sender):
+    """Testing: Adds a content to a conversation table"""
+    conversation = Conversation.query.get(conversation_id)
+    if not conversation:
+        return False
+    new_content = Content(content=content, sender=sender, conversation=conversation)
+    db.session.add(new_content)
+    db.session.commit()
+    return True
+
+
+# def check_password(email, password):
+#     user = User.query.filter_by(email=email).first()
+#     if user and user.password == password:
+#         return user
+#     else:
+#         return False
+
+
+# routes
 @app.route("/login", methods=["POST"])
 def login():
     posted_data = request.get_json()
     email = posted_data["email"]
     password = posted_data["password"]
-    user = User.query.filter_by(email=email).first()
-    if user and user.password == password:
-        username = user.username
-        user_id = user.user_id
-        token = generate_token(user.user_id)
-        return jsonify({"user_id": user_id, "username": username, "token": token}), 200
-    else:
-        return jsonify({"status": "Not found"}), 404
+    try:
+        user = User.query.filter_by(email=email).first()
+        if user and user.check_password(password):
+            token = generate_token(user.user_id)
+            return (
+                jsonify(
+                    {
+                        "user_id": user.user_id,
+                        "email": user.email,
+                        "username": user.username,
+                        "token": token,
+                    }
+                ),
+                200,
+            )
+        else:
+            return jsonify({"message": "Incorrect email or password"}), 401
+    except:
+        return jsonify({"message": "Not found"}), 404
 
 
 @app.route("/signup", methods=["POST"])
@@ -137,6 +165,7 @@ def signup():
             "message": "User created successfully",
             "token": token,
             "username": user.username,
+            "email": email,
         }
     )
 
@@ -206,11 +235,50 @@ def save():
         return jsonify({"message": "Saved successfully"}), 200
 
 
+@app.route("/update-user-data", methods=["GET", "POST"])
+def update_user_data():
+    if request.method == "POST":
+        posted_data = request.get_json()
+        print(type(posted_data))
+        user_id = posted_data["user_id"]
+        old_data = posted_data["origin_data"]
+        new_data = posted_data["new_data"]
+        user = User.query.filter_by(user_id=user_id).first()
+        if not user:
+            return jsonify({"message": "User not found"}), 404
+
+        # if password is empty, then change the username and email
+        if new_data["password"] == "" or old_data["password"] == "":
+            user.username = new_data.get("username", user.username)
+            user.email = new_data.get("email", user.email)
+
+        # else verify the user is exist, and change the password
+        else:
+            if user.check_password(old_data.get("password")):
+                user.set_password(new_data.get("password"))
+            else:
+                return jsonify({"message": "Incorrect password"}), 400
+
+        # Commit the changes to the database
+        db.session.commit()
+
+        return (
+            jsonify(
+                message="User data changed successfully",
+                username=user.username,
+                email=user.email,
+                user_id=user.user_id,
+            ),
+            200,
+        )
+    else:
+        return jsonify({"message": "Conversation not found"}), 404
+
+
 @app.route("/translate", methods=["GET", "POST"])
 def translate_to_en():
     if request.method == "POST":
         posted_data = request.get_json()
-        print("posted_data:.....", posted_data)
         conversations = posted_data["conversations"]
         lan_code = posted_data["lan_code"]
         translated_conversations = conversations
@@ -231,7 +299,6 @@ def get_conversations():
         return jsonify({"message": "Missing token"}), 401
     else:
         user_id = decode_token(token)
-        print("user_id:", user_id)
         user = User.query.filter_by(user_id=user_id).first()
         if not user:
             return jsonify({"message": "User not found"}), 404
@@ -252,7 +319,6 @@ def get_conversations():
                     "conversations": conversation_content,
                 }
                 data.append(conversation_data)
-            print("data:.....", data)
             return jsonify({"data": data}), 200
         else:
             return jsonify({"data": []})
@@ -284,6 +350,39 @@ def get_all_conversations():
         return jsonify({"data": []}), 200
 
 
+def generate_gpt_conversation(lan_code, topic, sentence_num, level, note):
+    
+    prompt = f"Language code: {lan_code}. Difficulty levels: {level}. Total sentences: {str(sentence_num)}. Topics: {topic}. Note: {note}."
+    
+    example = "[{\"sender\": \"A\", \"content\": \"...\"}, {\"sender\": \"B\", \"content\": \"...\"}]"
+
+    response = ai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": f"You are a multi-language generator which generates creative open conversations of A and B senders. The response format must follow valid and complete JSON format, which enclosed in double quotes, for example: {example}",
+            },
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+        temperature=1,
+        max_tokens=2000,
+        top_p=1,
+    )
+    completion = response.choices[0].message.content
+    print(completion)
+    conversations_array = json.loads(completion)
+    print(conversations_array)
+    try:
+        return conversations_array
+    except Exception as e:
+        print(e)
+        return e
+
+
 @app.route("/generate-conversation", methods=["POST"])
 def generate_conversation():
     posted_data = request.get_json()
@@ -291,8 +390,15 @@ def generate_conversation():
     topic = posted_data["topic"]
     sentence_num = posted_data["sentence_num"]
     level = posted_data["level"]
-    response = generate_gpt_conversation(lan_code, topic, sentence_num, level)
-    return jsonify(response)
+    note = None
+    try:
+        conversations = generate_gpt_conversation(
+            lan_code, topic, sentence_num, level, note
+        )
+        return jsonify(conversations), 200
+    except Exception as e:
+        print(e)
+        return jsonify({"response": "Request failed"}), 400
 
 
 @app.route("/generate-five", methods=["POST"])
@@ -300,79 +406,23 @@ def generate_five_conversation():
     posted_data = request.get_json()
     lan_code = posted_data["lan_code"]
     topic = posted_data["topic"]
+    conversations = posted_data["conversations"]
     sentence_num = 5
     level = "similar"
-    response = generate_gpt_conversation(lan_code, topic, sentence_num, level)
-    print(response)
-    return jsonify(response)
 
+    content_string = " ".join(
+        [conversation["content"] for conversation in conversations]
+    )
 
-def generate_token(user_id):
-    payload = {"user_id": user_id}
-    token = jwt.encode(payload, app.config["SECRET_KEY"], algorithm="HS256")
-    return token
-
-
-def decode_token(token):
+    note = f"previous conversation is about {content_string}."
     try:
-        payload = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
-        user_id = payload["user_id"]
-        print("Decoded payload:", payload)
-        return user_id
-    except jwt.ExpiredSignatureError:
-        print("Token has expired")
-        return None
-    except jwt.PyJWTError as e:
-        print("JWT decoding error:", str(e))
-        return None
-
-
-def translate_text(text, source_language, target_language):
-    translate_client = translate.Client.from_service_account_json(
-        GOOGLE_APPLICATION_CREDENTIALS
-    )
-    result = translate_client.translate(
-        text, source_language=source_language, target_language=target_language
-    )
-    return result["translatedText"]
-
-
-def generate_gpt_conversation(lan_code, topic, sentence_num, level):
-    language = googletrans.LANGUAGES[lan_code]
-    prompt = f'Generate a  {language} conversation. Difficulty levels: {level}. Total {str(sentence_num)} sentences. About {topic}. In Json format, ex:[{{"sender": "A","content": "...",}},]'
-    request_headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {GPT_APIKEY}",
-    }
-    request_data = {
-        "model": "text-davinci-003",
-        "prompt": prompt,
-        "max_tokens": 1000,
-        "temperature": 0.7,
-    }
-
-    response = requests.post(
-        GPT_API_ENDPOINT, headers=request_headers, json=request_data
-    )
-    if response.status_code == 200:
-        generated_conversations = response.json()["choices"][0]["text"]
-        stripped_res = generated_conversations.strip()
-        return stripped_res
-    else:
-        print(
-            f"Request failed with status code: {str(response.status_code), response.text}"
+        conversations = generate_gpt_conversation(
+            lan_code, topic, sentence_num, level, note
         )
-
-
-def add_content_to_conversation(conversation_id, content, sender):
-    """Testing: Adds a content to a conversation table"""
-    conversation = Conversation.query.get(conversation_id)
-    if not conversation:
-        return False
-    new_content = Content(content=content, sender=sender, conversation=conversation)
-    db.session.add(new_content)
-    db.session.commit()
-    return True
+        return jsonify(conversations), 200
+    except Exception as e:
+        print(e)
+        return jsonify({"response": "Request failed"}), 400
 
 
 @app.route("/add_content")
