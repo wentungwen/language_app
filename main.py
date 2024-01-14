36b1f7900fb14ceebcb5a727fb645bca140d6065
wentung <wentungwen@gmail.com>
2024-01-14 16:32:29 +0100

    restructure the requirements and main.py for deployment

--- main.py
+++ main.py
@@ -453,5 +453,5 @@ if __name__ == "__main__":
     # with app.app_context():
     #     db.create_all()
     # set the port for heroku
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
